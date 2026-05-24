c8bc605bcc7aa124a799aa50e17d5771f7f3bf1b
Gonzalo <gonzalovico05@gmail.com>
2026-05-24 12:14:04 +0200

    Push de prueba

--- src/app.js
+++ src/app.js
@@ -63,6 +63,7 @@ app.get("/", (req, res) => {
     <main>
       <h1>practica7-cicd</h1>
       <p>API REST desplegada automaticamente con GitHub Actions, Docker Hub y un VPS.</p>
+      <p>Prueba haciendo un cambio<p>
       <span class="status">Servidor funcionando</span>
       <h2>Endpoints disponibles</h2>
       <ul>
